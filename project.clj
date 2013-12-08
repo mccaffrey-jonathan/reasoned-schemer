f5bec9bddfaa636ee72fea175758638ba9b6c0cb
jmccaf <mccaffrey.jonathan@gmail.com>
2013-12-07 20:34:49 -0800

    inital project

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject default "0.1.0-SNAPSHOT"
+  :plugins [[lein-midje "3.0.0"]]
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+                 [midje "1.5.1"] 
+
+                 [org.clojure/clojure "1.5.1"]
+
+                 [org.clojure/core.logic "0.8.4"]])
